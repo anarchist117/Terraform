cc1597c4fae5afe1016528c0cd44c0f31b124b52
anarchist117 <146730762+anarchist117@users.noreply.github.com>
2025-05-14 01:16:49 +0300

    Update vpc.tf

--- Yandex/Kubernetes/vpc.tf
+++ Yandex/Kubernetes/vpc.tf
@@ -7,6 +7,7 @@ resource "yandex_vpc_subnet" "k8s_nodes" {
   folder_id = yandex_resourcemanager_folder.k8s.id
   name = "k8s_nodes"
   v4_cidr_blocks = ["10.252.0.0/16"]
+  route_table_id = yandex_vpc_route_table.nat-instance-route.id
   network_id     = yandex_vpc_network.k8s.id
 }
 
