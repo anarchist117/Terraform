9eedf8b584e3634074e6edc4e4ca96235b088578
anarchist117 <146730762+anarchist117@users.noreply.github.com>
2025-05-20 16:48:43 +0300

    Create cert-manager.tf

---
+++ Helm/cert-manager.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  version          = "v1.17.2"
+
+  set {
+    name  = "crds.enabled"
+    value = "true"
+  }
+}
