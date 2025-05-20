a65978bb6ecf18c7eb5188a59a090da69b51a3ef
anarchist117 <146730762+anarchist117@users.noreply.github.com>
2025-05-20 17:45:01 +0300

    Create dashboard.tf

---
+++ Helm/dashboard.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "kubernetes_dashboard" {
+  name             = "kubernetes-dashboard"
+  repository       = "https://kubernetes.github.io/dashboard/"
+  chart            = "kubernetes-dashboard"
+  namespace        = "kubernetes-dashboard"
+  create_namespace = true
+  version          = "7.12.0"
+}
